2b6af8105c3ae6a3cc4bab62a098ebbddc37dabd
rmi <1405733736@qq.com>
2020-09-07 22:09:03 +0800

    失败重试机制

--- src/main/java/com/bottle/pay/modules/biz/service/MerchantNoticeConfigService.java
+++ src/main/java/com/bottle/pay/modules/biz/service/MerchantNoticeConfigService.java
@@ -40,8 +40,8 @@ public class MerchantNoticeConfigService extends BottleBaseService<MerchantNotic
 //    @Autowired
 //    private RestTemplate restTemplate;
 
-//    @Async
-    public boolean sendNotice(Long orgId, Long merchantId,String billId) {
+    //    @Async
+    public boolean sendNotice(Long orgId, Long merchantId, String billId) {
         /**
          * 1。根据机构Id和商户ID拿到商户的回调配置  还有出款订单
          * 2。根据配置请求回调保存日志
@@ -52,16 +52,16 @@ public class MerchantNoticeConfigService extends BottleBaseService<MerchantNotic
         query.setMerchantId(merchantId);
         query.setBillId(billId);
         BillOutEntity billOutEntity = billOutMapper.selectOne(query);
-        if(billOutEntity == null){
-            log.warn("orgId:{},merchantId:{},订单ID:{} 没有找到出款订单",orgId,merchantId,billId);
+        if (billOutEntity == null) {
+            log.warn("orgId:{},merchantId:{},订单ID:{} 没有找到出款订单", orgId, merchantId, billId);
             throw new RRException("没有找到出款订单");
         }
-        if(billOutEntity.getBillStatus() != BillConstant.BillStatusEnum.Success.getCode()){
-            log.warn("orgId:{},merchantId:{},订单ID:{} 出款订单没有支付成功不能回调",orgId,merchantId,billId);
+        if (billOutEntity.getBillStatus() != BillConstant.BillStatusEnum.Success.getCode()) {
+            log.warn("orgId:{},merchantId:{},订单ID:{} 出款订单没有支付成功不能回调", orgId, merchantId, billId);
             throw new RRException("出款订单没有支付成功不能回调");
         }
-        if(billOutEntity.getNotice() == BillConstant.BillNoticeEnum.Noticed.getCode()){
-            log.warn("orgId:{},merchantId:{},订单ID:{} 出款订单已通知不能重复通知",orgId,merchantId,billId);
+        if (billOutEntity.getNotice() == BillConstant.BillNoticeEnum.Noticed.getCode()) {
+            log.warn("orgId:{},merchantId:{},订单ID:{} 出款订单已通知不能重复通知", orgId, merchantId, billId);
             throw new RRException("出款订单已回调成功不能重复回调");
         }
         //查询商户配置
@@ -70,54 +70,50 @@ public class MerchantNoticeConfigService extends BottleBaseService<MerchantNotic
         entity.setOrgId(orgId);
         entity.setCreateTime(null);
         MerchantNoticeConfigEntity config = mapper.selectOne(entity);
-        if(config == null){
-            log.warn("orgId:{},merchantId:{} 没有找到回调配置",orgId,merchantId);
+        if (config == null) {
+            log.warn("orgId:{},merchantId:{} 没有找到回调配置", orgId, merchantId);
             throw new RRException("没有找到回调配置");
         }
         String[] param = MerchantNoticeConfigEntity.DEFAULT_PARAMS;
-        if(StringUtils.isNotEmpty(config.getNoticeParams())){
-            param = StringUtils.split(config.getNoticeParams(),MerchantNoticeConfigEntity.SPLIT_CHAR);
+        if (StringUtils.isNotEmpty(config.getNoticeParams())) {
+            param = StringUtils.split(config.getNoticeParams(), MerchantNoticeConfigEntity.SPLIT_CHAR);
         }
-        Map<String,Object> map = Maps.newHashMap();
-        for(int i=0; i<param.length; i++){
+        Map<String, Object> map = Maps.newHashMap();
+        for (int i = 0; i < param.length; i++) {
             try {
-                Field field = ReflectionUtils.findField(billOutEntity.getClass(),param[i]);
-                Method m = billOutEntity.getClass().getMethod("get" + field.getName().substring(0,1).toUpperCase()+field.getName().substring(1,field.getName().length()));
-                Object val =  m.invoke(billOutEntity);
-                map.put(param[i],val);
+                Field field = ReflectionUtils.findField(billOutEntity.getClass(), param[i]);
+                Method m = billOutEntity.getClass().getMethod("get" + field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1, field.getName().length()));
+                Object val = m.invoke(billOutEntity);
+                map.put(param[i], val);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        map.put("orderNo",billOutEntity.getThirdBillId());
-        int times =3;
+        map.put("orderNo", billOutEntity.getThirdBillId());
+        int times = 3;
         BillOutEntity update = new BillOutEntity();
         update.setId(billOutEntity.getId());
-        do{
+        do {
             try {
-                HttpResult result = httpAPIService.doPost(config.getNoticeUrl(),map);
-                log.info("回调请求{},参数{},结果:{}",config.getNoticeUrl(),map,result.getBody());
-                if(result.getCode() == HttpStatus.SC_OK){
+                HttpResult result = httpAPIService.doPost(config.getNoticeUrl(), map);
+                log.info("回调请求{},参数{},结果:{}", config.getNoticeUrl(), map, result.getBody());
+                if (result.getCode() == HttpStatus.SC_OK) {
                     update.setNotice(BillConstant.BillNoticeEnum.Noticed.getCode());
                     int num = billOutMapper.updateByPrimaryKeySelective(update);
-                    log.info("出款订单:{}通知结果:{}",billOutEntity.getId(),num>0);
-                    times=-1;
+                    log.info("出款订单:{}通知结果:{}", billOutEntity.getId(), num > 0);
+                    times = -1;
                     return true;
                 }
             } catch (Exception e) {
                 e.printStackTrace();
-                log.warn("回调请求:{}异常:{}",config.getNoticeUrl(),e.getMessage());
-                if(e instanceof ConnectionPoolTimeoutException){
-                    log.warn("连接超时异常，重复请求");
-                    times--;
-                }else {
-                    times=-1;
-                }
+                log.warn("回调请求:{}异常:{}", config.getNoticeUrl(), e.getMessage());
+                log.warn("连接超时异常，重复请求");
+                times--;
             }
-        }while (times<=0);
+        } while (times > 0);
         update.setNotice(BillConstant.BillNoticeEnum.NoticeFailed.getCode());
         int num = billOutMapper.updateByPrimaryKeySelective(update);
-        log.info("出款订单:{}通知结果:{}",billOutEntity.getId(),num>0);
+        log.info("出款订单:{}通知结果:{}", billOutEntity.getId(), num > 0);
         return false;
     }
 
